25707ca14a6b2ce1d9c5ce5776f9b4e364cbbc4d
HeronNepomuceno <heronnepo1@gmail.com>
2023-03-01 20:56:10 -0300

    feat: add SimpleButton component

---
+++ src/components/buttons/SimpleButton.svelte
@@ -0,0 +1,62 @@
+<script>
+import Icon from '@iconify/svelte'
+export let action
+export let icon
+export let label
+export let theme
+export let type = 'button'
+export let url
+</script>
+
+{#if url}
+	<a href={url} class={`simple-button ${theme} target ripple`}>
+		{#if icon}
+			<Icon icon={icon} width="24" height="24"/>
+		{/if}
+		{label}
+	</a>
+{:else}
+	<button class={`simple-button ${theme} target ripple`} type={type} on:click={action}>
+		{#if icon}
+			<Icon icon={icon} width="24" height="24"/>
+		{/if}
+		{label}
+	</button>
+{/if}
+
+<style lang="stylus">
+@import '../../styles/'
+
+.simple-button
+	align-items center
+	border-radius 20px
+	display flex
+	font var(--label-large)
+	height 40px
+	justify-content center
+	white-space nowrap
+
+// 🎨 Themes
+.filled
+	background var(--primary)
+	color var(--on-primary)
+	padding 0 24px
+
+:global(.iconify)
+	margin 0 8px 0 -8px
+
+.tonal
+	background var(--secondary)
+	color var(--on-secondary)
+	padding 0 24px
+
+:global(.iconify)
+	margin 0 8px 0 -8px
+
+.standard
+	color var(--primary)
+	padding 0 12px
+
+:global(.iconify)
+	margin-right 8px
+</style>
