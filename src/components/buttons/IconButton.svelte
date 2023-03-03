3824b171093d2505a1ada88aa2afa108f413bd18
HeronNepomuceno <heronnepo1@gmail.com>
2023-03-03 10:59:50 -0300

    feat: add icon button component

---
+++ src/components/buttons/IconButton.svelte
@@ -0,0 +1,52 @@
+<script>
+	import Icon from '@iconify/svelte'
+	export let action
+	export let icon 
+	export let theme
+	export let url
+</script>
+
+{#if url}
+	<a href={url} class="target">
+		<span class={`icon-button ${theme} ripple`}>
+			{#if icon}
+				<Icon icon={icon} width="24" height="24"/>
+			{/if}
+		</span>
+	</a>
+{:else}
+	<button class="target" on:click={action}>
+		<span class={`icon-button ${theme} ripple`}>
+			{#if icon}
+				<Icon icon={icon} width="24" height="24"/>
+			{/if}
+		</span>
+	</button>
+{/if}
+
+<style lang="stylus">
+.target
+	place-content center
+	display grid
+	height 48px
+	width 48px
+
+.icon-button
+	place-content center
+	border-radius 50%
+	display grid
+	height 40px
+	width 40px
+
+// 🎨 Themes
+.filled
+	background var(--primary)
+	color var(--on-primary)
+
+.tonal
+	background var(--secondary)
+	color var(--on-secondary)
+
+.standard
+	color var(--primary)
+</style>  
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,6 @@
 <script>
   import FloatingActionButton from '../components/buttons/FloatingActionButton.svelte'
+	import IconButton from '../components/buttons/IconButton.svelte';
   import SimpleButton from "../components/buttons/SimpleButton.svelte"
 </script>
 
@@ -7,3 +8,4 @@
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 <SimpleButton icon="mdi:arrow-right" label="Click Here!" theme="filled"/>
 <FloatingActionButton icon="mdi:heart" theme="fixed"/>
+<IconButton icon="mdi:menu" theme="tonal"/>
