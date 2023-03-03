d3b467e2c768496490fda208caea559fa2284b28
HeronNepomuceno <heronnepo1@gmail.com>
2023-03-02 21:20:34 -0300

    refactor: create routes organization

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,7 @@
+<script>
+  import SimpleButton from "../components/buttons/SimpleButton.svelte"
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<SimpleButton label="Click Here!" theme="filled"/>
