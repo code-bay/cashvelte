a16df80dab8185729db9bdfc8e3f4af81ea89b1b
HeronNepomuceno <heronnepo1@gmail.com>
2023-03-03 10:55:47 -0300

    feat: add floating action button component

---
+++ src/components/buttons/FloatingActionButton.svelte
@@ -0,0 +1,35 @@
+<script>
+	import Icon from '@iconify/svelte'
+	export let action
+	export let icon
+	export let theme
+</script>
+
+<button class={`floating-action-button ${theme}`} on:click={action}>
+	{#if icon}
+		<Icon icon={icon} width="24" height="24"/>
+	{/if}
+</button>
+
+<style lang="stylus">
+.floating-action-button
+	align-items center
+	background var(--primary)
+	border-radius 16px
+	color var(--on-primary)
+	display flex
+	height 56px
+	justify-content center
+	width 56px
+
+// 🔧 Mods
+.extended
+	min-width 80px
+
+.fixed
+	bottom 16px
+	box-shadow rgba(#000, .24) 0px 3px 8px
+	right 16px
+	position fixed
+	z-index 1
+</style>  
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,9 @@
 <script>
+  import FloatingActionButton from '../components/buttons/FloatingActionButton.svelte'
   import SimpleButton from "../components/buttons/SimpleButton.svelte"
 </script>
 
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-<SimpleButton label="Click Here!" theme="filled"/>
+<SimpleButton icon="mdi:arrow-right" label="Click Here!" theme="filled"/>
+<FloatingActionButton icon="mdi:heart" theme="fixed"/>
