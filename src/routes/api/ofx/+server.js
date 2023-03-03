c820a696bda66b99934d11d84cf0c56857d428f2
HeronNepomuceno <heronnepo1@gmail.com>
2023-03-02 21:23:05 -0300

    feat: create ofx convert post endpoint

---
+++ src/routes/api/ofx/+server.js
@@ -0,0 +1,9 @@
+import {OFXParse} from "../../../../lib/server/ofx-utils"
+import { json } from '@sveltejs/kit';
+
+export async function POST({ request }) {
+	const raw_ofx = await request.text()
+	const parsed_ofx = OFXParse(raw_ofx)
+
+	return json(parsed_ofx);
+}
