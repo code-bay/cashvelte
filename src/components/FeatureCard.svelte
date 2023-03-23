d995d485e92f3e5d94ff235e0d5ce84ce95d9010
Katriel <katriel.developer@gmail.com>
2023-03-23 12:03:09 -0300

    feat(components): add FeatureCard

---
+++ src/components/FeatureCard.svelte
@@ -0,0 +1,22 @@
+<script>
+	import Icon from '@iconify/svelte'
+	export let description
+	export let icon
+	export let title
+</script>
+
+<div class="card">
+	<div class="symbol"><Icon icon={icon} width="24" height="24"/></div>
+	<div class="title">{title}</div>
+	<div class="description">{description}</div>
+</div>
+
+<style lang="stylus">
+.card
+	border-radius 12px
+	display flex
+	flex-direction column
+	
+.elevated
+	background var(--surface)
+</style>
