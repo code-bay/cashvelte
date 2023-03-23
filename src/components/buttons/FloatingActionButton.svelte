88b1e18a88883ac8439a77da26f8ea855978833f
Katriel <katriel.developer@gmail.com>
2023-03-23 12:12:18 -0300

    refactor(components): change FloatingActionButton props and color system

--- src/components/buttons/FloatingActionButton.svelte
+++ src/components/buttons/FloatingActionButton.svelte
@@ -1,31 +1,59 @@
 <script>
 	import Icon from '@iconify/svelte'
 	export let action
+	export let color
 	export let icon
-	export let theme
+	export let label
+	export let options
+	export let type
 </script>
 
-<button class={`floating-action-button ${theme}`} on:click={action}>
+<button
+	class={`floating-action-button ${color} ${options} ${type} ripple`}
+	on:click={action}
+>
 	{#if icon}
 		<Icon icon={icon} width="24" height="24"/>
+	{/if}
+	{#if label}
+		<span class="label">{label}</span>
 	{/if}
 </button>
 
 <style lang="stylus">
 .floating-action-button
 	align-items center
-	background var(--primary)
 	border-radius 16px
-	color var(--on-primary)
 	display flex
+	font var(--label-large)
 	height 56px
 	justify-content center
 	width 56px
 
-// 🔧 Mods
+// Mods ----- //
+// 1 - Color
+.surface
+	background var(--surface-4)
+	color var(--on-surface)
+
+.primary
+	background var(--primary-container)
+	color var(--on-primary-container)
+
+// 2 - Shape
+.round
+	border-radius 50%
+
+// 3 - Size
 .extended
 	min-width 80px
+	padding 0 16px
 
+.large
+	height 72px
+	width 72px
+
+// 4 - Position
 .fixed
 	bottom 16px
 	box-shadow rgba(#000, .24) 0px 3px 8px
