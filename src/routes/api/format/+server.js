2a88cf6e77bf64e8da63cc03b1e8b515efb284e4
HeronNepomuceno <heronnepo1@gmail.com>
2023-03-27 19:17:11 -0300

    feat: add transactionsByDate endpoint

--- src/routes/api/format/+server.js
+++ src/routes/api/format/+server.js
@@ -1,7 +1,25 @@
 import { convertTimestampToDate } from '$lib/server/ofx-utils.js'
+import { json } from '@sveltejs/kit'
 
-export async function GET() {
-	const date = convertTimestampToDate('20221102000000[-3:EST]') // ofx 
+export async function POST({ request }) {
+	const raw_json = await request.json()
+	const raw_transactions = raw_json.OFX.BANKMSGSRSV1.STMTTRNRS.STMTRS.BANKTRANLIST.STMTTRN
 
-	return new Response(date)
+	const transactionsByDate = {}
+
+	raw_transactions.forEach((elem) => {
+		const date = convertTimestampToDate(elem.DTPOSTED).toLocaleDateString()
+		if (!transactionsByDate[date]) {
+			transactionsByDate[date] = []
+		}
+		transactionsByDate[date].push(elem)
+	})
+
+	const dates = Object.keys(transactionsByDate)
+	dates.map((elem) => {
+		transactionsByDate[elem]
+	})
+
+
+	return json(transactionsByDate)
 }
