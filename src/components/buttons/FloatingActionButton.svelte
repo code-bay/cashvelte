aa588b887a239b879fc11296cc790c78ce289b12
Katriel <katriel.developer@gmail.com>
2023-04-10 21:09:19 -0300

    refactor(components): make FloatingActionButton support more elements types

--- src/components/buttons/FloatingActionButton.svelte
+++ src/components/buttons/FloatingActionButton.svelte
@@ -1,41 +1,72 @@
-<script>
+<script>	
 	import Icon from '@iconify/svelte';
-	export let action;
+	//export let accept
+	export let action = () => {};
 	export let color;
 	export let icon;
 	export let label;
-	export let options;
+	export let position = "";
+	export let size = "";
 	export let type;
+	export let href;
 </script>
 
-<button class={`floating-action-button ${color} ${options} ${type} ripple`} on:click={action}>
-	{#if icon}
-		<Icon {icon} width="24" height="24" />
-	{/if}
-	{#if label}
-		<span class="label">{label}</span>
-	{/if}
-</button>
+{#if href}
+	<a {href} class={`floating-action-button ${color} ${position} ${size} ripple target`}>
+		{#if icon}
+			<Icon {icon} width="24" height="24" />
+		{/if}
+		{#if label}
+			<span class="label">{label}</span>
+		{/if}
+	</a>
+{:else if type === "button"}
+	<button {type} class={`floating-action-button ${color} ${position} ${size} ripple target`} use:action>
+		{#if icon}
+			<Icon {icon} width="24" height="24" />
+		{/if}
+		{#if label}
+			<span class="label">{label}</span>
+		{/if}
+	</button>
+{:else}
+	<label class={`floating-action-button ${color} ${position} ${size} ripple target`} use:action>
+		<input {type} />
+		{#if icon}
+			<Icon {icon} width="24" height="24" />
+		{/if}
+		{#if label}
+			<span class="label">{label}</span>
+		{/if}
+	</label>
+{/if}
 
 <style lang="stylus">
 .floating-action-button
 	align-items center
 	border-radius 16px
-	display flex
+	cursor pointer
+	display inline-flex
 	font var(--label-large)
 	height 56px
 	justify-content center
-	width 56px
+	min-width 56px
+	white-space nowrap
+	max-width 500px
 
 // Mods ----- //
 // 1 - Color
-.surface
+.neutral
 	background var(--surface-4)
 	color var(--on-surface)
 
+.surface
+	background var(--surface-4)
+	color var(--primary)
+
 .primary
-	background var(--primary-container)
-	color var(--on-primary-container)
+	background var(--primary)
+	color var(--on-primary)
 
 // 2 - Shape
 .round
@@ -43,18 +74,22 @@
 
 // 3 - Size
 .extended
-	min-width 80px
+	max-width 500px
 	padding 0 16px
 
-.large
-	height 72px
-	width 72px
+	:global(.iconify)
+		margin-right 12px
 
 // 4 - Position
 .fixed
-	bottom 16px
+	bottom 96px
 	box-shadow rgba(#000, .24) 0px 3px 8px
 	right 16px
 	position fixed
 	z-index 1
+
+// 5 - Fixes
+
+input[type="file"]
+	display none
 </style>
